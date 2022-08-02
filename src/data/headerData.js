b1084f4e1b29fc14a0e48c9fe391925d1e800081
Vishal0899 <97458281+Vishal0899@users.noreply.github.com>
2022-08-02 13:03:07 +0530

    commited

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,5 @@ export const headerData = {
   title: "A Full Stack Web Developer",
   desciption: "Life is 10% what happens to us and 90% how we react to it.",
   image: profileImg,
-  // resumePdf: "https://drive.google.com/file/d/1mqzBsNCeMtNzf_0ZPf0MtxLMLeZf74yh/view?usp=sharing"
-  // resumePdf: resume,
+  resumePdf: resume,
 };

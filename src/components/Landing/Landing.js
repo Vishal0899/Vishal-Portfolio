0d63abc9d6c492c478d4a87565390b6e9f0bd25c
Vishal0899 <97458281+Vishal0899@users.noreply.github.com>
2022-08-05 11:53:16 +0530

    commited

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -170,7 +170,7 @@ function Landing() {
                             {headerData.resumePdf && (
                                 <a
                                     href={headerData.resumePdf}
-                                    download='resume'
+                                    // download='resume'
                                     target='_blank'
                                     rel='noreferrer'
                                 >

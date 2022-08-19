61d623854bae0becd11e21388e763641d3eb1f37
Vishal0899 <97458281+Vishal0899@users.noreply.github.com>
2022-08-19 11:28:27 +0530

    commited

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
-import resume from "../assets/pdf/Resume.pdf";
+// import resume from "../assets/pdf/Resume.pdf";
 import profileImg from "../assets/png/profileImg.jpeg";
 export const headerData = {
   name: "Vishal Pokale",
   title: "A Full Stack Web Developer",
   desciption: "Life is 10% what happens to us and 90% how we react to it.",
   image: profileImg,
-  resumePdf: resume,
+  resumePdf: "https://drive.google.com/file/d/12v1W8wf93uJsMWbqfD0i4c9adLTxScaO/view?usp=sharing",
 };
